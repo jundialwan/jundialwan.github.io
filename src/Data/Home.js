6abea0c24f2be34294a19a618265c4a74c588475
jundialwan <jundi.alwan@gmail.com>
2017-07-18 17:57:35 +0700

    Update home content

---
+++ src/Data/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const home = () => { 
+  return (
+    <div style={{ textAlign: 'justify' }}>
+      <div style={{ borderLeft: '4px solid #444', paddingLeft: '12px' }}>
+        <em>
+          I have specific interest in JavaScript and Frontend web development, 
+          proficient in HTML, CSS, PHP, JavaScript, and frontend framework and
+          two years experience in web project management handling various project.
+        </em>
+      </div> <br />
+      Welcome. <br /><br />
+      I know, this website looks boring. Dull color. Black white (grey).
+      I keep it for simplicity. *i have a bad taste at design lol <br /><br />
+
+      My bad, let me introduce myself. <br />
+      Hi, my name is <strong>Jundi</strong>. 
+      I&#39;m a computer science student and web developer living in Indonesia.
+      I have specific interest in JavaScript and Frontend web development,
+      proficient in HTML, CSS, PHP, and JavaScript.
+      I have experience in Web developing since 2014
+      and also two years experience in web project management handling various project.
+      I might not be great at building beautiful website 
+      but you can count on me at more complex website or system
+      <br /><br />
+      I could say I have a reliable communication and leadership skill. 
+      I&#39;d volunteer and leading in various non-profit event and organization, 
+      working with team consist of hundreds people.
+    </div>
+  ); 
+};
+
+export default home;
